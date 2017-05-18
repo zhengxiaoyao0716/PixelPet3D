06225b3f2170019dc27763767f35059c66801e5a
zhengxiaoyao0716 <1499383852@qq.com>
2017-05-18 18:06:24 +0800

    windows调试socket及其不便。准备移交到Coding远程开发

--- static/script/websocket.js
+++ static/script/websocket.js
@@ -1,17 +1,30 @@
 {
+    if (location.host == "") {
+        throw new Error("WebSocket was closed.");
+    }
+
+    var handlers = {
+        "/info/get": function (data) {
+            // expect: %s: %s\nAuthor: %s\nAddress: %s
+            var infoPanel = document.querySelector("#infoPanel");
+            data.split("\n").forEach(function(line) {
+                var p = document.createElement("p");
+                infoPanel.appendChild(p);
+                p.textContent = line;
+            });
+        },
+        "text": console.log,
+        "action": console.log,
+    };
+
     var ws = new WebSocket("ws://localhost:5000/ws");
     // ws.addEventListener("open", (e) => {
     //     console.log(e);
     // });
     ws.addEventListener("message", function (e) {
         return (function (json) {
-            var event = json["event"];
-            var data = json["data"];
-            ({
-                "init": console.log,
-                "text": console.log,
-                "action": console.log,
-            })[event](data);
+            var handler = handlers[json["event"]];
+            handler ? handler(json["data"]) : console.warn("No handler for:", json);
         })(JSON.parse(e.data));
     });
     // ws.addEventListener("close", (e) => {
