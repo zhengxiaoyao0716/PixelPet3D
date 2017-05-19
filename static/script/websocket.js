3db7719a24d2d9de4d1fc509423ec1c3e247fd84
zhengxiaoyao0716 <1499383852@qq.com>
2017-05-19 04:31:04 +0000

    C,Python,Js三端通信完成

--- static/script/websocket.js
+++ static/script/websocket.js
@@ -1,5 +1,5 @@
 {
-    if (location.host == "") {
+    if (location.host === "") {
         throw new Error("WebSocket was closed.");
     }
 
@@ -7,28 +7,28 @@
         "/info/get": function (data) {
             // expect: %s: %s\nAuthor: %s\nAddress: %s
             var infoPanel = document.querySelector("#infoPanel");
-            data.split("\n").forEach(function(line) {
+            data.split("\n").forEach(function (line) {
                 var p = document.createElement("p");
                 infoPanel.appendChild(p);
                 p.textContent = line;
             });
         },
-        "text": console.log,
+        "text": function () { console.log("Pet said 'pong'"); },
         "action": console.log,
     };
 
-    var ws = new WebSocket("ws://localhost:5000/ws");
-    // ws.addEventListener("open", (e) => {
-    //     console.log(e);
-    // });
+    var ws = new WebSocket("ws" + location.origin.slice(4) + "/ws");
+
+    ws.addEventListener("open", function (e) {
+        var interval = setInterval(function () { ws.send("Hello"); console.log("U said 'ping'."); }, 3000);
+        ws.addEventListener("close", function (e) {
+            clearInterval(interval);
+        });
+    });
     ws.addEventListener("message", function (e) {
         return (function (json) {
             var handler = handlers[json["event"]];
             handler ? handler(json["data"]) : console.warn("No handler for:", json);
         })(JSON.parse(e.data));
     });
-    // ws.addEventListener("close", (e) => {
-    //     console.log(e);
-    // });
-    setTimeout(function () { return ws.send("Hello"); }, 1000);
 }
