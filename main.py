0ec2ca99b0205a5a34c7ec9121cb595d522da4a5
zhengxiaoyao0716 <1499383852@qq.com>
2017-06-08 23:17:35 +0800

    添加多客户端广播

--- main.py
+++ main.py
@@ -25,29 +25,44 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     """WebSocket"""
+    waiters = set()
+
+    @classmethod
+    def join_waiters(cls, self):
+        """加入一个连接客户端"""
+        cls.waiters.add(self)
+
+    @classmethod
+    def remove_waiters(cls, self):
+        """加入一个连接客户端"""
+        cls.waiters.remove(self)
+
+    @classmethod
+    def broadcast(cls, event, data):
+        """向所有人广播"""
+        for waiter in cls.waiters:
+            waiter.write_message({'event': event, 'data': data})
 
     def check_origin(self, origin):
         return True
 
     def open(self, *args, **kwargs):
+        self.join_waiters(self)
         for path in [
-            '/screen/render',
-            '/warn',
-            '/error',
+                '/screen/render',
+                '/warn',
+                '/error',
         ]:
-            (lambda path:
-                core_conn.on(path,
-                             lambda data: self.write_message({'event': path, 'data': data}))
-             )(path)
+            (lambda path: core_conn.on(
+                path,
+                lambda data: self.broadcast(path, data)))(path)
 
         @gen.coroutine
         def get_info():
             """获取信息"""
-            data = yield core_conn.invoke("/info/get", "%s: %s\nAuthor: %s\nAddress: %s")
-            self.write_message({
-                "event": "/info/get",
-                "data": data
-            })
+            data = yield core_conn.invoke(
+                "/info/get", '%s: %s\nAuthor: %s\nAddress: %s\n{"pet": "PiPi"}')
+            self.broadcast("/info/get", data)
             core_conn.emit('/screen/active')
         IOLoop.current().spawn_callback(get_info)
 
@@ -61,23 +76,16 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
             def invoke():
                 """获取信息"""
                 data = yield core_conn.invoke(path, message)
-                self.write_message({
-                    "event": path,
-                    "data": data
-                })
+                self.broadcast(path, data)
             IOLoop.current().spawn_callback(invoke)
         else:
             if path.startswith('emit://'):
                 path = path[7:]
             core_conn.emit(path, message)
-            self.write_message({
-                "event": path + '/emit',
-                "data": 'fin'
-            })
+            self.broadcast(path + '/emit', 'fin')
 
     def on_close(self):
-        # TODO clean
-        pass
+        self.remove_waiters(self)
 
 
 if __name__ == '__main__':
